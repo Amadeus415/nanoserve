85b3cf4729f6368cf4d4e414d0304295d2259b67
Amadeus Colenbrander <amadeus@Amadeuss-MacBook-Pro.local>
2026-08-21 17:59:41 -0700

    chore: scaffold project (package skeleton, pyproject, gitignore)

---
+++ nanoserve/__init__.py
@@ -0,0 +1,7 @@
+"""nanoserve: a minimal, hackable LLM serving stack.
+
+Built as a learning foundation for inference/serving engineering:
+engine -> server -> benchmarks -> plots. Small enough to read top to bottom.
+"""
+
+__version__ = "0.1.0"
